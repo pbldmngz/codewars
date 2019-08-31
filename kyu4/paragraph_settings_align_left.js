823147b4fb0e25e317ee32bb0f1f1a05dba1dca5
Rhythm <32307513+pbldmngz@users.noreply.github.com>
2019-08-31 09:18:11 -0700

    this kata is not kyu4

--- kyu4/paragraph_settings_align_left.js
+++ kyu4/paragraph_settings_align_left.js
@@ -1,33 +1,30 @@
-function nicePara(str, len) {
-    _ = [];
-    str = str.split(" ");
-    f = "";
-    for (var i = 0; i < str.length; i++){
-        if (str[i].length + 1 + f.length <= len){ //No debería haber mayor problema aquí
-            f += str[i] + " ";
+function nicePara(inText, lineLength) {
+    inText = inText.trim();
+    var _ = [];
+    var i;
+    while (inText.length > 0) {
+        if (inText.length <= lineLength) {
+            _.push(inText);
+            inText = "";
         } else {
-            if (len - f.length <= 2){ //Si quedan menos de dos espacios
-                if (str[i].length >= 2){ // Y la palabra cabe ahí
-                    _.push(f + str[i]);
-                    f = "";
-                } else { //Y la palabra NO cabe ahí
-                    _.push(f.trim());
-                    f = "";
+            i = lineLength;
+            if (inText[i] != " ") {
+                while (!inText.slice(i - 3, i + 1).match(/[0-9a-zA-Z]{4}/) 
+                && (inText[i - 1] != "-" && inText[i - 1] != " ")) {
+                    i--;
                 }
-            } else if (str[i].includes("-")){ //Si la palabra-esta-formada-así //Y hay huecos para ponerla
-                console.log("Situación que aún tengo que comprender: " + str[i]);
-            } else if (str[i].length > len - f.length && str[i].length >= 4){ //Quedan 3+ espacios, la palabra no cabe ahí y mide 4+
-                f += str[i].slice(0, len - f.length - 1) + "-";
-                _.push(f);
-                f = str[i].slice(len - f.length - 1) + " ";
+            }
+            while (inText[i - 1] == " ") {
+                i--;
+            }
+            if (inText[i] == " " | inText[i - 1] == "-") {
+                _.push(inText.slice(0, i));
+                inText = inText.slice(i).trim();
             } else {
-                console.log("situación que no comprendo #2: " + str[i]);
+                _.push(inText.slice(0, i - 1) + "-");
+                inText = inText.slice(i - 1);
             }
         }
     }
     return _.join("\n");
 }
-var tx = "A lot of different plans are marchingteytye-under-dddedthe-baesessic income banner. If-you-want-to-know-what-their-true-colours-are, then don’t be fooled by the all-things-to-all-people language used about them. Look at the bottom line. How are they going to pay for it? The devil is in the detail.";
-console.log(nicePara(tx, 15));
-//Viendo los resultados, asumo que hay algo que me falta en el código que ya tengo que se come las 3 letras finales de una frase que no cabe
-//Y bueno... procesar los guiones
