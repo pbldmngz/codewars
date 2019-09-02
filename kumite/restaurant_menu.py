91043a07a440a615ba8c41cd34d04769114f5d8f
Rhythm <32307513+pbldmngz@users.noreply.github.com>
2019-09-02 07:21:37 -0700

    Update restaurant_menu.py

--- kumite/restaurant_menu.py
+++ kumite/restaurant_menu.py
@@ -2,8 +2,8 @@ def frm(data, mx):
     if len(data) == 0: return None
     _, mx = [], mx - len(str(max([d[1] for d in data]))) - 1
     for d in data:
-        if (mx-len(d[0])) <= 0: return None
+        if (mx-len(d[0].strip(" "))) <= 0: return ""None""
         _.append("{}{}${}".format(d[0].upper().strip(),
-        "."*(mx-(len(d[0].lstrip(" ")) + len(d[0].rstrip(" ")) - len(d[0]))),
+        "."*(mx - len(d[0].strip(" "))),
         str(d[1])))
     return "\n".join(_)
