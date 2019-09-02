eb3718fdc45c33f682e3e98bec48c88b73ade425
Rhythm <32307513+pbldmngz@users.noreply.github.com>
2019-09-02 00:02:52 -0700

    Create restaurant_menu.py

---
+++ kumite/restaurant_menu.py
@@ -0,0 +1,9 @@
+def frm(data, mx):
+    if len(data) == 0: return None
+    _, mx = [], mx - len(str(max([d[1] for d in data]))) - 1
+    for d in data:
+        if (mx-len(d[0])) <= 0: return None
+        _.append("{}{}${}".format(d[0].upper().strip(),
+        "."*(mx-(len(d[0].lstrip(" ")) + len(d[0].rstrip(" ")) - len(d[0]))),
+        str(d[1])))
+    return "\n".join(_)
