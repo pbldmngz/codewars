d3f9efb4ade7a90525ea23b5bc34af331c2b7e0a
Roushan SIngh <38443304+r-tron18@users.noreply.github.com>
2021-05-21 18:07:26 +0530

    Wrong solution for Make A Spiral
    The Code fails for test cases like size=12 and many more.  This is one of the working code as verified by me by submitting it in codewars

--- kyu3/make_a_spiral.py
+++ kyu3/make_a_spiral.py
@@ -1,41 +1,78 @@
-def spiralize(size):
-    a = []
-    for i in range(size):
-        a.append([])
-    for n in range(size):
-        for i in range(size):
-            a[n].append([])
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
 
-    for i in range(0, size):
-        for j in range(0, size):
-            a[i][j] = 1
-    i = 1
-    j = size - 2
-    while i < size:
-        h = i - 1
-        while h <= j:
-            a[i][h] = 0
-            h += 1
-        
-        h = i + 1
-        while h <= j:
-            a[h][j] = 0
-            h += 1
-            
-        h = j - 1
-        while h >= i:
-            a[j][h] = 0
-            h -= 1
-            
-        h = j - 1
-        while h > i + 1:
-            a[h][i] = 0
-            h -= 1
-        i += 2
-        j -= 2
-    
-    if size % 2 == 0 and size > 2:
-    #I know this is kinda weird, i already dit the kata in ruby and had no problem
-        if size % 6 == 0: a[size//2][size//2] = 1
-        else: a[size//2][size//2] = 0 
-    return a
+def getNextStep(cx, cy, dire):
+    nx = cx + dx[dire]
+    ny = cy + dy[dire]
+    return nx, ny
+
+def spiralize(size):
+    M = [[0]*size for i in range(size)]
+    cx, cy = 0, 0
+    ## directions
+    '''
+    dire East = 0
+    dire South = 1
+    dire West = 2
+    dire North = 3
+    '''
+    def isValid(a, b):
+        return 0<=a<size and 0<=b<size
+
+    def isValNxt(X, Y, Dire):
+        nx, ny = getNextStep(X, Y, Dire)
+        return isValid(nx, ny)
+
+    def isZeroNxt(X, Y, Dire):
+        nx, ny = getNextStep(X, Y, Dire)
+        return M[nx][ny] == 0
+
+    def check(X, Y, Dire):
+        if isValNxt(X, Y, Dire):
+            if isZeroNxt(X, Y, Dire):
+                return True
+            return False
+        return True
+
+    def doubleCheck(X, Y, Dire):
+        ## check the next 2 cells should be
+        ## invalid invalid
+        ## 0       invalid
+        ## 0       0
+        nx, ny = getNextStep(cx, cy, Dire)
+        nnx, nny = getNextStep(nx, ny, Dire)
+        val1 = isValid(nx, ny)
+        val2 = isValid(nnx, nny)
+        if val1==False and val2==False:
+            return True
+        if M[nx][ny] == 0 and val2 == False:
+            return True
+        if M[nx][ny] == 0 and M[nnx][nny] == 0:
+            return True
+        return False
+
+    dire = 0 ## intially go east
+    while True:
+        ## can keep current cell
+        ## if next cell is invalid or 0 in current direction
+        ## and next cell is invalid or valid and 0 in Direction (dire + 1) % 4
+        if check(cx, cy, dire) and check(cx, cy, (dire+1)%4):
+            M[cx][cy] = 1
+        nx, ny = getNextStep(cx, cy, dire)
+        if isValid(nx, ny) == False:
+            dire = (dire + 1) % 4
+            nx, ny = getNextStep(cx, cy, dire)
+            if isValid(nx, ny) and check(cx, cy, dire):
+                cx, cy = nx, ny
+            else:
+                break
+        else:
+            if doubleCheck(cx, cy, dire):
+                cx, cy = getNextStep(cx, cy, dire)
+            else:
+                dire = (dire + 1) % 4
+                if doubleCheck(cx, cy, dire):
+                    cx, cy = getNextStep(cx, cy, dire)
+                else:
+                    break
+    return M
